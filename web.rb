98441dbbf3b442f749b947030d669e50ae147f22
sacsar <sacsar@gmail.com>
2013-01-07 16:12:19 -0600

    Getting slim going, hopefully.
    Also add a bit of restaurant set up.

--- web.rb
+++ web.rb
@@ -1,5 +1,17 @@
 require 'sinatra'
+require 'slim'
+
+class Restaurant
+  attr_accessor :name, :location, :fast, :cheap
+  
+  def initialize (name, location, fast = False, cheap = False)
+    @name = name
+    @location = location
+    @fast = fast
+    @cheap = cheap
+  end
+end
 
 get '/' do
-  "Hello, world"
+  slim :index
 end
