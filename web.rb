1fee42db17ccb72c05cf8fbbf556442802cb4a99
sacsar <sacsar@gmail.com>
2013-01-08 15:37:13 -0600

    Pretty much everything. Selector works, added some css.

--- web.rb
+++ web.rb
@@ -4,7 +4,7 @@ require 'slim'
 class Restaurant
   attr_accessor :name, :location, :fast, :cheap
   
-  def initialize (name, location, fast = False, cheap = False)
+  def initialize (name, location, fast = false, cheap = false)
     @name = name
     @location = location
     @fast = fast
@@ -12,6 +12,30 @@ class Restaurant
   end
 end
 
+bona=Restaurant.new("Bona","wash")
+jasmine=Restaurant.new("Jasmine Orchard","wash")
+mesa=Restaurant.new("Mesa","dinkytown",true,true)
+wally=Restaurant.new("Wally's","dinkytown")
+camdi=Restaurant.new("Camdi","dinkytown")
+
+restaurantlist=[bona,jasmine,mesa,wally]
+
 get '/' do
   slim :index
+end
+
+post '/lunch' do
+  consider=Array.new
+  if params[:location] == "any"
+    consider = restaurantlist
+  else
+    restaurantlist.each do |restaurant|
+      if params[:location] == restaurant.location
+        consider.push(restaurant)
+      end
+    end
+  end
+  r=rand(consider.size)
+  @choice = consider[r]
+  slim :lunch
 end
